67e206ad87ce6f3cfd5d7fa0beddcab57d68bbf3
Declanwhx <declanwee@gmail.com>
2025-02-07 10:42:20 +0100

    Add files via upload

---
+++ installation_scripts/installation_allegro.sh
@@ -0,0 +1,152 @@
+#!/bin/sh
+
+# Change versions accordingly (Note: the developers of Allegro and NequIP have adopted the naming convention "main" and "develop" instead of the usualy "stable")
+allegro_vers=develop
+allegro_pair_vers=main
+nequip_vers=0.6.0
+
+# Loading modules
+module load 2023r1-gcc11
+module load openmpi/4.1.4
+module load miniconda3
+module load cuda/11.6
+module load cmake/3.24.3
+module load fftw/3.3.10
+
+# Conda environment initialization
+conda remove -n allegro_$allegro_vers --all -y
+conda create -n allegro_$allegro_vers python=3.10 -y
+conda activate allegro_$allegro_vers
+
+################################################################## ALLEGRO INSTALLATION ##################################################################
+rm -rf allegro
+mkdir allegro
+cd allegro
+
+# Install NumPY and Pytorch mainly, do not mess with these versions, 2.x does not work with NequIP/Allegro and likewise, Pytorch should ideally be 1.11.0
+conda install numpy=1.26.4 scipy=1.11.3 -c conda-forge -y
+conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch -y
+
+# Install weights&biases, this is primarily for visual tracking of validation and training errors
+pip install wandb
+
+# Install NequIP
+pip install nequip==$nequip_vers
+
+# Install Allegro (Note: Allegro was built as an extension of NequIP)
+rm -rf allegro
+git clone https://github.com/mir-group/allegro.git
+cd allegro
+git checkout $allegro_vers
+pip install .
+
+# Navigate back to main folder
+cd ..
+
+################################################################## LAMMPS INSTALLATION ##################################################################
+# Location env variables
+lammps_path=/scratch/dwee/software/allegro/lammps_allegro
+
+# Git clone stable version of LAMMPS
+rm -rf lammps_allegro
+git clone https://github.com/lammps/lammps.git lammps_allegro
+cd lammps_allegro
+git checkout stable
+
+# Navigate back to main folder
+cd ..
+
+# Git clone stable version of pair_allegro and patch
+rm -rf pair_allegro
+git clone https://github.com/mir-group/pair_allegro
+cd pair_allegro
+git checkout $allegro_pair_vers
+./patch_lammps.sh $lammps_path 
+
+# Navigate back to LAMMPS folder
+cd $lammps_path
+
+# Installing Libtorch
+wget https://download.pytorch.org/libtorch/cu113/libtorch-cxx11-abi-shared-with-deps-1.11.0%2Bcu113.zip && unzip -q libtorch-cxx11-abi-shared-with-deps-1.11.0+cu113.zip
+
+# Installing cudnn, version specifically for CUDA 11.x
+conda install -c conda-forge cudnn=8.9.2.26 -y
+
+# Exporting cuDNN paths
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+export CUDNN_INCLUDE_DIR=$CONDA_PREFIX/include
+export CUDNN_LIBRARY=$CONDA_PREFIX/lib
+
+# CMake script to enable all packages and configurations equivalent to the provided bash script
+# PACKages not enabled
+# 1) PYTHON -- useful (Required python-dev)
+# 2) KIM -- maybe useful
+# 3) LATBOLTZ -- maybe useful
+# 4) VERONI -- maybe useful
+# 7) GPU -- not useful
+# 6) ADIOS -- useful (Required Double precision FFTW3)
+# 7) VTK -- maybe useful (Would be hard to install)
+# 8) PLUMED -- maybe useful (Required libgsl-dev: sudo apt-get install libgsl-dev)
+# 9) SCAFACOS -- maybe useful (Required libgsl-dev: sudo apt-get install libgsl-dev)
+# 10) RHEO -- maybe useful (Required libgsl-dev: sudo apt-get install libgsl-dev)
+# 11) NETCDF -- maybe useful
+
+# List of all packages to enable
+ALL_PACKAGES=(
+#  ASPHERE
+#  BODY
+#  CLASS2
+#  DIPOLE
+#  EXTRA-COMMAND
+#  EXTRA-COMPUTE
+#  EXTRA-DUMP
+#  EXTRA-FIX
+#  EXTRA-MOLECULE
+#  EXTRA-PAIR
+#  GRANULAR
+#  INTERLAYER
+  KOKKOS
+#  KSPACE
+#  MANYBODY
+#  MOLECULE
+#  MOLFILE
+#  OPENMP
+#  OPT
+#  RIGID
+#  SHOCK
+#  TALLY
+)
+
+rm -rf build
+mkdir build
+cd build
+
+ADD_PACKAGES=""
+# Append all packages as -D flags to the cmake command
+for PKG in "${ALL_PACKAGES[@]}"; do
+  ADD_PACKAGES+=" -D PKG_${PKG}=yes"
+done
+# KOKKOS_SETTINGS="-D Kokkos_ARCH_SPR=ON"
+KOKKOS_SETTINGS=" -D Kokkos_ENABLE_OPENMP=ON"
+KOKKOS_SETTINGS+=" -D FFT_KOKKOS=FFTW3"
+KOKKOS_SETTINGS+=" -D Kokkos_ARCH_AMPERE80=ON"
+KOKKOS_SETTINGS+=" -D Kokkos_ENABLE_CUDA=ON"
+KOKKOS_SETTINGS+=" -D FFT_KOKKOS=cuFFT"
+
+# set path to libtorch install
+ALLEGRO_SETTINGS="-DCMAKE_PREFIX_PATH=$lammps_path/libtorch"
+ALLEGRO_SETTINGS+=" -DMKL_INCLUDE_DIR="$CONDA_PREFIX/include""
+
+CMAKE_PREP="cmake $ADD_PACKAGES $KOKKOS_SETTINGS $ALLEGRO_SETTINGS ../cmake"
+$CMAKE_PREP
+CMAKE_BUILD="cmake --build . -j 6"
+# CMAKE_BUILD="cmake --build . -j $(($SLURM_NTASKS * $SLURM_CPUS_PER_TASK))"
+$CMAKE_BUILD
+
+cd ..
+
+conda deactivate
+
+
+
+
