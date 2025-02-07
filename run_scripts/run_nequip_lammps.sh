ab5d146a0696a566a49501e355cfa3b7dcaa9059
Declanwhx <declanwee@gmail.com>
2025-02-07 11:07:30 +0100

    Add files via upload

---
+++ run_scripts/run_nequip_lammps.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+#SBATCH --job-name="nequip_run_gpu"
+#SBATCH --partition=gpu-a100-small
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --account=research-me-pe
+#SBATCH --mail-type=FAIL
+
+# Expected directory structure:
+#
+# project/
+# ├── input_files/
+# │   ├── si.data
+# │   └── Si_data
+# │       └── /Si_data
+# ├── input.yaml
+# ├── inputlammps
+# └── run_nequip.sh
+
+# project -> e.g H2O
+# inputlammps -> LAMMPS input settings file
+# input.yaml -> NequIP input settings file
+# si.data -> Initializing snapshot of system for MD
+# Si_data -> Training and validation data for NequIP
+
+# Change run no. accordingly
+run_no=1
+
+lmp_path=/scratch/dwee/software/nequip/lammps_nequip/build
+
+# Get modules
+module load 2023r1-gcc11
+module load openmpi/4.1.4
+module load miniconda3
+module load cuda/11.6
+module load cmake/3.24.3
+module load fftw/3.3.10
+
+# Add conda correctly to the paths, this will be needed in runscripts later
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+export CUDNN_INCLUDE_DIR=$CONDA_PREFIX/include
+export CUDNN_LIBRARY=$CONDA_PREFIX/lib
+
+# Activate your environment
+conda activate nequip 
+
+# Copy to temporary directory
+start1=$(date +%s)
+echo "Starting to copy"
+cp -r ${SLURM_SUBMIT_DIR} /tmp/${SLURM_JOBID}
+cd /tmp/${SLURM_JOBID}
+stop1=$(date +%s)
+echo "Copying done, simulation starting, time elapsed is $(($stop1-$start1)) seconds"
+
+############################################################# TRAINING ############################################################
+srun --output=training.out nequip-train ./input.yaml
+echo "Training done"
+####################################################################################################################################
+
+############################################################ PRE-DEPLOY ############################################################
+srun --output=pre-deploy.out nequip-deploy build --train-dir results/Si/run-Si/ si-deployed.pth
+echo "Deployment done"
+####################################################################################################################################
+
+############################################################## DEPLOY ##############################################################
+# run the simulation with ntasks*cpus-per-task cores
+srun --output=deploy.out $lmp_path/lmp -in ./inputlammps # -sf kk -k on gpus 1 -pk kokkos newton on neigh full
+####################################################################################################################################
+
+# Delete old output files, comment out if you want to retain them
+rm -rf output_files*
+mkdir output_files_$run_no
+mv results wandb si-deployed.pth si.rdf log.lammps training.out pre-deploy.out deploy.out output_files_$run_no
+
+echo "Simulation done, copying back" 
+# copy back
+rm slurm-${SLURM_JOBID}.out
+rsync -a "$(pwd -P)/" ${SLURM_SUBMIT_DIR}
+rm -rf /tmp/${SLURM_JOBID}
+
+seff ${SLURM_JOBID}
