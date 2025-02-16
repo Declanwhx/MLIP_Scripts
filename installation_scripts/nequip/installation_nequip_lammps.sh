c292fa966db9211faf800e2e3d4f08da9d90ccb4
Declanwhx <declanwee@gmail.com>
2025-02-16 21:30:40 +0100

    Add files via upload

--- installation_scripts/nequip/installation_nequip_lammps.sh
+++ installation_scripts/nequip/installation_nequip_lammps.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#SBATCH --job-name="install_nequip"
+#SBATCH --partition=gpu-a100-small
+#SBATCH --time=04:00:00
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --account=research-me-pe
+
 # THIS FILE INSTALLS BOTH THE NEQUIP MLIP AND PAIR_NEQUIP LAMMPS
 
 # NOTE: CLONE THE "INSTALLATION_SCRIPTS" FOLDER TO YOUR SOFTWARE FOLDER AND JUST RUN THE INSTALLATION SCRIPT, THERE IS NO NEED TO MOVE THE SCRIPT AROUND, IT WILL NAVIGATE OUT OF THIS FOLDER TO INSTALL IN THE SOFTWARE FOLDER.
@@ -33,71 +42,95 @@ nequip_pair_vers=main
 lammps_vers=stable
 
 # Location env variable. PLEASE CHANGE THIS TO YOUR INTENDED INSTALL LOCATION.
-lammps_path=$HOME/software/nequip/lammps_nequip
+lammps_path=/scratch/dwee/software/nequip/lammps_nequip
 
-# Loading modules
+# ======================
+# 🔧 Load System Modules
+# ======================
 module load 2023r1-gcc11
-module load openmpi/4.1.4
 module load miniconda3
+module load openmpi/4.1.4
 module load cuda/11.6
 module load cmake/3.24.3
 module load fftw/3.3.10
 
-# Conda environment initialization
-conda remove -n nequip_$nequip_vers --all -y
-conda create -n nequip_$nequip_vers python=3.10 -y
-conda activate nequip_$nequip_vers
+# ===========================
+# 🔧 Create Conda Environment
+# ===========================
+conda remove -n nequip --all -y
+conda create -n nequip python=3.10 -y
+conda activate nequip
 
-################################################################## NEQUIP INSTALLATION ##################################################################
-# Navigate out of script file first
 cd ../../
-
 rm -rf nequip
 mkdir nequip
 cd nequip
 
-# Install NumPY and Pytorch mainly, do not mess with these versions, 2.x does not work with NequIP/Allegro and likewise, Pytorch should ideally be 1.11.0
+# =======================
+# 🔧 Install Dependencies
+# =======================
 conda install numpy=1.26.4 scipy=1.11.3 -c conda-forge -y
 conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch -y
-
-# Install weights&biases, this is primarily for visual tracking of validation and training errors
+conda install mkl-include -y
+conda install -c conda-forge cudnn=8.9.2.26 -y
 pip install wandb
-
-# Install NequIP
 pip install nequip==$nequip_vers
 
-################################################################## LAMMPS INSTALLATION ##################################################################
-# Git clone stable version of LAMMPS
-rm -rf lammps_nequip
+# =================
+# 🔧 Clone LAMMPS
+# =================
 git clone https://github.com/lammps/lammps.git lammps_nequip
 cd lammps_nequip
 git checkout $lammps_vers
-
-# Navigate back to main folder
 cd ..
 
-# Git clone stable version of pair_allegro and patch
-rm -rf pair_nequip
+# =====================================
+# 🔧 Clone Pair_Nequip and Patch LAMMPS
+# =====================================
 git clone https://github.com/mir-group/pair_nequip
 cd pair_nequip
 git checkout $nequip_pair_vers
 ./patch_lammps.sh $lammps_path 
-
-# Navigate back to LAMMPS folder
 cd $lammps_path
 
-# Installing Libtorch
+# =============
+# 🔧 Clone OCTP
+# =============
+git clone https://github.com/omoultosEthTuDelft/OCTP.git
+cp OCTP/*.h OCTP/*.cpp $lammps_path/src
+
+# ===================
+# 🔧 Install Libtorch
+# ===================
 rm -rf libtorch*
 wget https://download.pytorch.org/libtorch/cu113/libtorch-cxx11-abi-shared-with-deps-1.11.0%2Bcu113.zip && unzip -q libtorch-cxx11-abi-shared-with-deps-1.11.0+cu113.zip
 
-# Installing cudnn, version specifically for CUDA 11.x
-conda install -c conda-forge cudnn=8.9.2.26 -y
+# ===================================================================
+# 🔧 Remove Conflicting Conda Libraries in cuDNN & Module Loaded CUDA 
+# ===================================================================
+rm -rf $CONDA_PREFIX/lib/libgomp*
+rm -rf $CONDA_PREFIX/lib/libcuda*
+rm -rf $CONDA_PREFIX/lib/libcudart*
+rm -rf $CONDA_PREFIX/lib/libcufft*
+rm -rf $CONDA_PREFIX/lib/libcurand*
+rm -rf $CONDA_PREFIX/lib/libcublas*
+rm -rf $CONDA_PREFIX/lib/libnvrtc*
+rm -rf $CONDA_PREFIX/lib/libcusolver*
+rm -rf $CONDA_PREFIX/lib/libcusparse*
+rm -rf $CONDA_PREFIX/lib/libnvToolsExt*
 
-# Exporting cuDNN paths
+# Set CUDA module paths
+export CUDA_HOME=/beegfs/apps/generic/cuda-11.6
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+# Add cuDNN from Conda
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 export CUDNN_INCLUDE_DIR=$CONDA_PREFIX/include
-export CUDNN_LIBRARY=$CONDA_PREFIX/lib
+export CUDNN_LIBRARY=$CONDA_PREFIX/lib/libcudnn.so
 
+# ==================================
+# 🔧 Build LAMMPS with Kokkos + CUDA
+# ==================================
 # CMake script to enable all packages and configurations equivalent to the provided bash script
 # PACKages not enabled
 # 1) PYTHON -- useful (Required python-dev)
@@ -114,28 +147,28 @@ export CUDNN_LIBRARY=$CONDA_PREFIX/lib
 
 # List of all packages to enable
 ALL_PACKAGES=(
-#  ASPHERE
-#  BODY
-#  CLASS2
-#  DIPOLE
-#  EXTRA-COMMAND
-#  EXTRA-COMPUTE
-#  EXTRA-DUMP
-#  EXTRA-FIX
-#  EXTRA-MOLECULE
-#  EXTRA-PAIR
-#  GRANULAR
-#  INTERLAYER
+  ASPHERE
+  BODY
+  CLASS2
+  DIPOLE
+  EXTRA-COMMAND
+  EXTRA-COMPUTE
+  EXTRA-DUMP
+  EXTRA-FIX
+  EXTRA-MOLECULE
+  EXTRA-PAIR
+  GRANULAR
+  INTERLAYER
   KOKKOS
-#  KSPACE
-#  MANYBODY
-#  MOLECULE
-#  MOLFILE
-#  OPENMP
-#  OPT
-#  RIGID
-#  SHOCK
-#  TALLY
+  KSPACE
+  MANYBODY
+  MOLECULE
+  MOLFILE
+  OPENMP
+  OPT
+  RIGID
+  SHOCK
+  TALLY
 )
 
 rm -rf build
@@ -147,25 +180,30 @@ ADD_PACKAGES=""
 for PKG in "${ALL_PACKAGES[@]}"; do
   ADD_PACKAGES+=" -D PKG_${PKG}=yes"
 done
-# KOKKOS_SETTINGS="-D Kokkos_ARCH_SPR=ON"
-KOKKOS_SETTINGS=" -D Kokkos_ENABLE_OPENMP=ON"
-KOKKOS_SETTINGS+=" -D FFT_KOKKOS=FFTW3"
-KOKKOS_SETTINGS+=" -D Kokkos_ARCH_AMPERE80=ON"
-KOKKOS_SETTINGS+=" -D Kokkos_ENABLE_CUDA=ON"
+
+# Kokkos Configuration (NOTE: KOKKOS IS CASE-SENSITIVE!!!)
+KOKKOS_SETTINGS="-D Kokkos_ENABLE_CUDA=ON"
+KOKKOS_SETTINGS+=" -D Kokkos_ENABLE_OPENMP=ON"
+KOKKOS_SETTINGS+=" -D Kokkos_ARCH_AMPERE80=ON"  
 KOKKOS_SETTINGS+=" -D FFT_KOKKOS=cuFFT"
 
-# set path to libtorch install
+# Ensure cuBLAS and cuDNN are properly linked
+COMPILER_SETTINGS="-D CMAKE_INCLUDE_PATH=$CONDA_PREFIX/include;/beegfs/apps/generic/cuda-11.6/include"
+COMPILER_SETTINGS+=" -D CMAKE_LIBRARY_PATH=$CONDA_PREFIX/lib;/beegfs/apps/generic/cuda-11.6/targets/x86_64-linux/lib"
+COMPILER_SETTINGS+=" -D CUDA_CUBLAS_LIBRARIES='/beegfs/apps/generic/cuda-11.6/targets/x86_64-linux/lib/libcublas.so;/beegfs/apps/generic/cuda-11.6/targets/x86_64-linux/lib/libcublasLt.so'"
+COMPILER_SETTINGS+=" -D CUDA_CUDNN_LIBRARIES='$CONDA_PREFIX/lib/libcudnn.so'"
+
+# Allegro Settings
 ALLEGRO_SETTINGS="-DCMAKE_PREFIX_PATH=$lammps_path/libtorch"
 ALLEGRO_SETTINGS+=" -DMKL_INCLUDE_DIR="$CONDA_PREFIX/include""
 
-CMAKE_PREP="cmake $ADD_PACKAGES $KOKKOS_SETTINGS $ALLEGRO_SETTINGS ../cmake"
+# Build LAMMPS
+CMAKE_PREP="cmake $ADD_PACKAGES $COMPILER_SETTINGS $KOKKOS_SETTINGS $ALLEGRO_SETTINGS ../cmake"
 $CMAKE_PREP
-CMAKE_BUILD="cmake --build . -j 6"
-# CMAKE_BUILD="cmake --build . -j $(($SLURM_NTASKS * $SLURM_CPUS_PER_TASK))"
+CMAKE_BUILD="cmake --build . -j 8"
 $CMAKE_BUILD
 
 cd ..
-
 conda deactivate
 
 
