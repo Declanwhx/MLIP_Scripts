83b7d476c6073dd9f5603c3b95b901a7de18463c
Declanwhx <declanwee@gmail.com>
2025-03-08 11:52:58 +0100

    Update installation_pytorch_cuda_distributed.sh

--- installation_scripts/deepmd/installation_pytorch_cuda_distributed.sh
+++ installation_scripts/deepmd/installation_pytorch_cuda_distributed.sh
@@ -1,3 +1,11 @@
-
+#!/bin/sh
+#SBATCH --job-name="install_pytorch"
+#SBATCH --partition=gpu-a100-small
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=5G
+#SBATCH --account=research-me-pe
 
 spack install -j10 --reuse py-torch +cuda +distributed cuda_arch=80
