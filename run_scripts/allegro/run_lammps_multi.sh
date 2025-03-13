bccd16737738c8ef342f244bfad88cbdf78cdc0c
Declan Wee Hong Xiang Declan <dwee@login01.delftblue.tudelft.nl>
2025-03-13 17:12:45 +0100

    updated run script

--- run_scripts/allegro/run_lammps_multi.sh
+++ run_scripts/allegro/run_lammps_multi.sh
@@ -1,90 +1,85 @@
 #!/bin/sh
-#SBATCH --job-name="allegro_run_10_r_multi_gpu"
+#SBATCH --job-name="allegro lammps run"
 #SBATCH --partition=gpu-a100
-#SBATCH --time=02:00:00
+#SBATCH --time=00:25:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=2
 #SBATCH --gpus-per-task=1
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=12G
 #SBATCH --account=research-me-pe
 #SBATCH --mail-type=FAIL
 #SBATCH --output=mpi-out.%j
 #SBATCH --error=mpi-err.%j
 
-# Expected directory structure:
-#
-# project/
-# ├── input_files/
-# │   ├── si.data
-# │   └── Si_data
-# │       └── /Si_data
-# ├── input.yaml
-# ├── inputlammps
-# └── run_allegro.sh
+lmp_path=~/software/allegro_lammps/lammps_allegro/build
 
-# project -> e.g H2O
-# inputlammps -> LAMMPS input settings file
-# input.yaml -> Allegro input settings file
-# si.data -> Initializing snapshot of system for MD
-# Si_data -> Training and validation data for Allegro
+# ------------------------------ Load Modules ------------------------------ #
+module use ~/software/spack/share/spack/lmod/linux-rhel8-x86_64/Core
+module use ~/software/spack/share/spack/lmod/linux-rhel8-x86_64/openmpi/4.1.6-h2uag4k/Core
 
-lmp_path=/scratch/dwee/software/allegro_lammps/lammps_allegro/build
-
-# Load Modules
 module load 2024r1
 module load miniconda3
-module load cuda/11.6
-module load cudnn/8.7.0.84-11.8
-module load py-numpy/1.24.1
-module load py-scipy/1.11.3
 module load openmpi/4.1.6
+module load py-torch/1.11.0
+module load py-scipy/1.11.3
+module load py-kiwisolver/1.4.5
+module load py-sympy/1.11.1
+module load py-mpmath/1.2.1
+module load py-pillow/10.0.0
+module load py-packaging/23.0
+module load py-matplotlib/3.7.1
+module load py-cycler/0.11.0
+module load py-contourpy/1.0.7
+module load py-fonttools/4.39.4
+module load py-wandb/0.13.9
 module load cmake/3.27.7
 module load fftw/3.3.10_openmp_True
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+# ------------------------------ Export MPI & CUDA Settings ------------------------------ #
 export CUDA_VISIBLE_DEVICES=$(echo $SLURM_JOB_GPUS | tr ',' '\n' | paste -sd ',')
+export OMPI_MCA_pml=ob1
+export OMPI_MCA_btl_smcuda_use_cuda_ipc=1
+export OMPI_MCA_btl=self,vader,smcuda
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
-export SLURM_MPI_TYPE=pmix  # Ensure correct MPI type
-export CUDA_LAUNCH_BLOCKING=1  # Debugging (optional)
 
 echo "SLURM assigned GPUs: $SLURM_JOB_GPUS"
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 
-# Activate your environment
-conda activate allegro 
+# ------------------------------ Activate Conda Environment ------------------------------ #
+conda activate allegro_lammps
 
-# Copy to temporary directory
+# ------------------------------ Copy to Temporary Directory ------------------------------ #
 start1=$(date +%s)
-echo "Starting to copy" >> slurm-${SLURM_JOB_ID}.out 2>&1
+echo "Starting to copy"
 cp -r ${SLURM_SUBMIT_DIR} /tmp/${SLURM_JOBID}
 cd /tmp/${SLURM_JOBID}
-stop1=$(date +%s) 
-echo "Copying done, simulation starting, time elapsed is $(($stop1-$start1)) seconds" >> slurm-${SLURM_JOB_ID}.out 2>&1
+stop1=$(date +%s)
+echo "Copying done, simulation starting, time elapsed is $(($stop1-$start1)) seconds"
 
-############################################################## DEPLOY ##############################################################
-# run the simulation with ntasks*cpus-per-task cores
-previous=$(nvidia-smi --query-gpu=utilization.gpu,memory.used --format=csv | tail -n +2)
-
+# ------------------------------ Run LAMMPS Simulation ------------------------------ #
 srun --mpi=pmix \
-     --ntasks=1 \
-     --gpus-per-task=1 \
-     --cpus-per-task=1 \
+     --ntasks=${SLURM_NTASKS} \
+     --gpus-per-task=${SLURM_GPUS_PER_TASK} \
+     --cpus-per-task=${SLURM_CPUS_PER_TASK} \
      --gpu-bind=none \
      "$lmp_path/lmp" \
      -in ./nvt_simulation.in \
-     -k on g 1 t 1 \
+     -k on g ${SLURM_NTASKS} t ${SLURM_CPUS_PER_TASK} \
      -sf kk \
-     -pk kokkos neigh full comm device cuda/aware off
+     -pk kokkos neigh full comm device cuda/aware on
 
-nvidia-smi --query-gpu=utilization.gpu,memory.used --format=csv | grep -v -F "$previous" >> slurm-${SLURM_JOB_ID}.out
-####################################################################################################################################
+# ------------------------------ Move Output Files ------------------------------ #
+mkdir ${SLURM_JOB_PARTITION}_${SLURM_NTASKS}G_${SLURM_CPUS_PER_TASK}T
+mv cuda.h mpi-* slurm* sys* volume.dat ${SLURM_JOB_PARTITION}_${SLURM_NTASKS}G_${SLURM_CPUS_PER_TASK}T
 
-echo "Simulation done, copying back" >> slurm-${SLURM_JOB_ID}.out 2>&1
-# copy back
+# ------------------------------ Copy Back and Cleanup ------------------------------ #
+echo "Simulation done, copying back" 
 rsync -a "$(pwd -P)/" ${SLURM_SUBMIT_DIR}
 rm -rf /tmp/${SLURM_JOBID}
 rm ${SLURM_SUBMIT_DIR}/slurm-${SLURM_JOBID}.out
 
 seff ${SLURM_JOBID}
+
--- run_scripts/allegro/run_training_pre_deployment.sh
+++ run_scripts/allegro/run_training_pre_deployment.sh
@@ -1,66 +1,76 @@
 #!/bin/sh
-#SBATCH --job-name=<JOB NAME>
+#SBATCH --job-name="allegro train"
 #SBATCH --partition=gpu-a100
-#SBATCH --time=08:00:00
+#SBATCH --time=04:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=6
 #SBATCH --gpus-per-task=1
-#SBATCH --mem-per-cpu=5G
+#SBATCH --mem-per-cpu=25G
 #SBATCH --account=research-me-pe
 #SBATCH --mail-type=FAIL
 
-lmp_path=/scratch/dwee/software/allegro_lammps/lammps_allegro/build
+# ------------------------------ Load Modules ------------------------------ #
+module use ~/software/spack/share/spack/lmod/linux-rhel8-x86_64/Core
+module use ~/software/spack/share/spack/lmod/linux-rhel8-x86_64/openmpi/4.1.6-h2uag4k/Core
 
-# Load Modules
 module load 2024r1
 module load miniconda3
-module load cuda/11.6
-module load cudnn/8.7.0.84-11.8
-module load py-numpy/1.24.1
-module load py-scipy/1.11.3
 module load openmpi/4.1.6
+module load py-torch/1.11.0
+module load py-scipy/1.11.3
+module load py-kiwisolver/1.4.5
+module load py-sympy/1.11.1
+module load py-mpmath/1.2.1
+module load py-pillow/10.0.0
+module load py-packaging/23.1
+module load py-matplotlib/3.7.1
+module load py-cycler/0.11.0
+module load py-contourpy/1.0.7
+module load py-fonttools/4.39.4
+module load py-wandb/0.13.9
 module load cmake/3.27.7
 module load fftw/3.3.10_openmp_True
 
-echo "SLURM assigned GPUs: $SLURM_JOB_GPUS"
-echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+# ------------------------------ Activate Environment ------------------------------ #
+conda activate allegro_lammps
 
-# Activate your environment
-conda activate allegro 
-
-# Copy to temporary directory
+# ------------------------------ Copy to Temporary Directory ------------------------------ #
 start1=$(date +%s)
-echo "Starting to copy" >> slurm-${SLURM_JOB_ID}.out 2>&1
+echo "📂 Starting to copy files..."
 cp -r ${SLURM_SUBMIT_DIR} /tmp/${SLURM_JOBID}
 cd /tmp/${SLURM_JOBID}
-stop1=$(date +%s) 
-echo "Copying done, simulation starting, time elapsed is $(($stop1-$start1)) seconds" >> slurm-${SLURM_JOB_ID}.out 2>&1
+stop1=$(date +%s)
+echo "✅ Copying complete. Time elapsed: $(($stop1 - $start1)) seconds."
 
-############################################################# TRAINING ############################################################
-srun --output=training.out nequip-train ./input.yaml >> slurm-${SLURM_JOB_ID}.out 2>&1
-echo "Training done" >> slurm-${SLURM_JOB_ID}.out 2>&1
-####################################################################################################################################
+# ------------------------------ TRAINING ------------------------------ #
+echo "🚀 Starting training..."
+srun --output=training.out nequip-train ./input.yaml
+echo "✅ Training complete."
 
-############################################################ EVALUATION  ############################################################
-srun nequip-evaluate --train-dir results/H2O/run-H2O --output predicted.xyz
-echo "Evaluation tests done" >> slurm-${SLURM_JOB_ID}.out 2>&1
-####################################################################################################################################
+# ------------------------------ EVALUATION (Optional) ------------------------------ #
+# echo "🔍 Starting evaluation..."
+# srun nequip-evaluate --train-dir results/H2O/run-H2O --batch-size 1 --output ./predicted.extxyz
+# echo "✅ Evaluation complete."
 
-############################################################ PRE-DEPLOY ############################################################
-srun --output=pre-deploy.out nequip-deploy build --train-dir results/H2O/run-H2O/ h2o-deployed.pth >> slurm-${SLURM_JOB_ID}.out 2>&1
-echo "Deployment done" >> slurm-${SLURM_JOB_ID}.out 2>&1
-####################################################################################################################################
+# ------------------------------ PRE-DEPLOYMENT ------------------------------ #
+echo "⚙️  Starting model deployment..."
+srun --output=pre-deploy.out nequip-deploy build --train-dir results/H2O/run-H2O/ h2o-deployed.pth
+echo "✅ Deployment complete."
 
-# Delete old output files, comment out if you want to retain them
-rm -rf training_files
-mkdir training_files
-mv results wandb h2o.rdf log.lammps training.out pre-deploy.out *.dat training_files
+# ------------------------------ Organize Output Files ------------------------------ #
+echo "📁 Organizing output files..."
+rm -rf output_files*
+mkdir output_files
+mv results wandb h2o.rdf log.lammps training.out pre-deploy.out *.dat output_files
 
-echo "Simulation done, copying back" >> slurm-${SLURM_JOB_ID}.out 2>&1
-# copy back
+# ------------------------------ Copy Back and Clean Up ------------------------------ #
+echo "🔄 Copying results back..."
 rsync -a "$(pwd -P)/" ${SLURM_SUBMIT_DIR}
 rm -rf /tmp/${SLURM_JOBID}
+
+# Uncomment if you want to remove the SLURM output file
 # rm ${SLURM_SUBMIT_DIR}/slurm-${SLURM_JOBID}.out
 
 seff ${SLURM_JOBID}
+
